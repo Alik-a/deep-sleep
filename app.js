795166a06cf01bdee51a63978f1a4a1ecc1f3f9b
Alik-a <155586026+Alik-a@users.noreply.github.com>
2024-05-17 00:37:09 +0300

    more on readFileSync
    trying out the encoding option

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const { readFileSync, writeFileSync } = require('fs')
 
-const first = readFileSync('./contents/first-text.txt' , 'utf8')
+const first = readFileSync('./contents/first-text.txt' )
 const second = readFileSync('./contents/second-text.txt' , 'utf8')
 
-console.log(first, second)
+// console.log(first, second)
 
-writeFileSync(
-    './contents/result-sync-text.txt',
-    'Here is the result: ${first}, {second}'
-)
+writeFileSync('./contents/result-sync-text.txt', `Here is the result: ${first}, ${second}`)
+
+
+
+// (   
+//     './contents/result-sync2-text.txt',
+//     'Here is the result: ${fisrt}, ${second}',
+// )
