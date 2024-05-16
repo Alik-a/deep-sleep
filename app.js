b4f7c7a281b3b7b96692da67475cbb6743d64aa1
Alik-a <155586026+Alik-a@users.noreply.github.com>
2024-05-17 00:14:26 +0300

    basic first modules
    added :
    modules of Os, Path and FS(Sync)

---
+++ app.js
@@ -0,0 +1,11 @@
+const { readFileSync, writeFileSync } = require('fs')
+
+const first = readFileSync('./contents/first-text.txt' , 'utf8')
+const second = readFileSync('./contents/second-text.txt' , 'utf8')
+
+console.log(first, second)
+
+writeFileSync(
+    './contents/result-sync-text.txt',
+    'Here is the result: ${first}, {second}'
+)
